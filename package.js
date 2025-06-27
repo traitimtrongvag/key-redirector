83bcd6a3a11a5afaaec5d8750d160df6dada9dcd
traitimtrongvag <tbinh831@gmail.com>
2025-06-28 01:08:46 +0700

    Update package.js

--- package.js
+++ package.js
@@ -2,6 +2,12 @@
   "name": "key-redirector",
   "version": "1.0.0",
   "type": "module",
+  "engines": {
+    "node": ">=18.x"
+  },
+  "scripts": {
+    "start": "node index.js"
+  },
   "dependencies": {
     "@supabase/supabase-js": "^2.39.3"
   }
